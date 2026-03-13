9a5cf205240ca6b5b1b4bb877fff1b1e7f96069a
srilathakolli19-hub <srilathakolli19@gmail.com>
2026-03-13 20:42:17 +0530

    Update eda.sql

--- eda.sql
+++ eda.sql
@@ -80,3 +80,24 @@ end py_change
 from yearly_product_sales
 order by product_name,order_year;
 
+
+--part to whole analysis
+
+with category_sales as (
+select 
+p.category as category,
+sum(f.sales_amount) as total_sales
+from gold.fact_sales f
+left join gold.dim_products p
+on p.product_key=f.product_key
+group by p.category)
+
+select 
+category,
+total_sales,
+SUM(total_sales) over() as overall_sales,
+concat(round((cast(total_sales as float)/SUM(total_sales) over())*100 ,2),'%') as percentage_of_total
+from category_sales
+order by percentage_of_total desc;
+
+
