22c62ed2add1d7c7bdaebd9b745d182aa75c3e6d
srilathakolli19-hub <srilathakolli19@gmail.com>
2026-03-13 21:11:26 +0530

    Update eda.sql

--- eda.sql
+++ eda.sql
@@ -104,3 +104,24 @@ from category_sales
 order by percentage_of_total desc;
 
 
+--data segmentation
+--segment products based on cost ranges and then group products to which segment they belong to
+with product_segments as(
+select
+product_key,
+product_name,
+case when cost<100 then 'below 100'
+     when cost between 100 and 500 then '100-500'
+     when cost between 500 and 1000 then '500-1000'
+     else 'above 1000'
+end cost_range
+from gold.dim_products)
+
+select 
+cost_range,
+COUNT(product_key) as total_products
+from product_segments
+group by cost_range
+order by total_products desc;
+
+
