f049ec56192695991eda880706070941d16d1c36
srilathakolli19-hub <srilathakolli19@gmail.com>
2026-03-15 12:33:05 +0530

    Update change_over_time.sql

--- change_over_time.sql
+++ change_over_time.sql
@@ -1,1 +1,29 @@
+use DataWarehouse;
+go
+
+
+--change over time analysis(months)
+
+select
+DATETRUNC(month,order_date) as order_date,
+SUM(sales_amount) as total_sales,
+COUNT(distinct customer_key) as total_customers,
+SUM(quantity) as total_quantity
+from gold.fact_sales
+where order_date is not null
+group by DATETRUNC(month,order_date)
+order by order_date
+
+
+--change over time analysis(years)
+
+select
+DATETRUNC(year,order_date) as order_date,
+SUM(sales_amount) as total_sales,
+COUNT(distinct customer_key) as total_customers,
+SUM(quantity) as total_quantity
+from gold.fact_sales
+where order_date is not null
+group by DATETRUNC(year,order_date)
+order by order_date
 
