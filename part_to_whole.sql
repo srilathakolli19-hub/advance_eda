955ac71d6bfe593215c7fa1f3b8ba7745345b876
srilathakolli19-hub <srilathakolli19@gmail.com>
2026-03-15 12:42:06 +0530

    Update part_to_whole.sql

--- part_to_whole.sql
+++ part_to_whole.sql
@@ -1,1 +1,20 @@
+--part to whole analysis
+--which categories contribute most to the overall sales
+
+with category_sales as (
+select 
+p.category as category,
+sum(f.sales_amount) as total_sales
+from gold.fact_sales f
+left join gold.dim_products p
+on p.product_key=f.product_key
+group by p.category)
+
+select 
+category,
+total_sales,
+SUM(total_sales) over() as overall_sales,
+concat(round((cast(total_sales as float)/SUM(total_sales) over())*100 ,2),'%') as percentage_of_total
+from category_sales
+order by percentage_of_total desc;
 
